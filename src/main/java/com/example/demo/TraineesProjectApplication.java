0f0862e304b728d5f683d08e3274deb28f229ac4
Angelica Villanueva <amvaarenas@hotmail.com>
2019-12-10 17:14:46 -0600

    hola a todos

---
+++ src/main/java/com/example/demo/TraineesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineesProjectApplication.class, args);
+	}
+
+}
