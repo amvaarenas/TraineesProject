1774f44e36a49a476288091569f99e691466034c
Roberto Rdz <roberto.rdz.m@outlook.com>
2019-12-11 12:21:23 -0600

    Updated Course annotations

--- src/main/java/com/example/demo/entities/Course.java
+++ src/main/java/com/example/demo/entities/Course.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "course")
 public class Course {
 	@Column(name = "name")
 	String name;
