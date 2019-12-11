ed14d402b51049c90cc7f8a4c16a5d496ea67b3e
Miguel G.Cantón <chinogcantontec@gmail.com>
2019-12-11 11:44:41 -0600

    Bug fixed, Course Service no registered as  Service for spring, @Service tag added to the file CourseService.java

--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -8,10 +8,12 @@ import com.example.demo.dao.StudentRepository;
 import com.example.demo.entities.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 /**
  * CourseService
  */
+@Service
 public class CourseService {
 
     @Autowired
